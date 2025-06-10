42b2cfcadc64921d9064984f12d45928f3354947
valentincharlone <valencharlone@gmail.com>
2025-06-10 01:24:06 -0300

    feat: refactor components for improved structure and functionality

--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -1,11 +1,21 @@
+import { useState } from "react";
+
+// https://bytegrad.com/course-assets/projects/rmtdev/api/data?search=react
+
 export default function SearchForm() {
+  const [searchText, setSearchText] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
   return (
-    <form action="#" className="search">
-      <button type="submit">
+    <form onSubmit={handleSubmit} className="search">
+      <button>
         <i className="fa-solid fa-magnifying-glass"></i>
       </button>
 
       <input
+        value={searchText}
+        onChange={(e) => setSearchText(e.target.value)}
         spellCheck="false"
         type="text"
         required
